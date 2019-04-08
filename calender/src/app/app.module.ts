9446032227f3376a67bf5c0ee8c3819a81ca5421
Tianju Zhou <zhou.tianj@husky.neu.edu>
2019-04-08 11:08:19 -0400

    add four components

--- calender/src/app/app.module.ts
+++ calender/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DailyScheduleComponent } from './daily-schedule/daily-schedule.component';
+import { WeeklyScheduleComponent } from './weekly-schedule/weekly-schedule.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    NavbarComponent,
+    DailyScheduleComponent,
+    WeeklyScheduleComponent
   ],
   imports: [
     BrowserModule,
