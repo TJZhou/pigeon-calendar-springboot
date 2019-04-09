3953eafd901514936e4b48b43a7945ab27ec2181
Tianju Zhou <zhou.tianj@husky.neu.edu>
2019-04-08 20:41:31 -0400

    add calender

--- calender/src/app/calendar/calendar.component.ts
+++ calender/src/app/calendar/calendar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-calendar',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
-
-  constructor() { }
+  public date = moment();
+  public dateArr;
+  constructor() {}
 
   ngOnInit() {
+    this.dateArr = this.createCalendar(this.date);
   }
 
+  createCalendar(month) {
+    const firstDay = moment(month).startOf('M');
+    const days = Array.apply(null, { length: month.daysInMonth() + 1 })
+      .map(Number.call, Number)
+      .slice(1);
+    for (let n = 0; n < firstDay.weekday(); n++) {
+      days.unshift(null);
+    }
+    return days;
+  }
 }
