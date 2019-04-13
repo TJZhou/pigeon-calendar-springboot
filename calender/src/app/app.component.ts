02bbca11173a919a31e13021b5598d89c0afc3ed
Tianju Zhou <zhou.tianj@husky.neu.edu>
2019-04-12 23:50:57 -0400

    update weekly schedule

--- calender/src/app/app.component.ts
+++ calender/src/app/app.component.ts
@@ -1,34 +1,61 @@
 import { CalendarComponent } from './calendar/calendar.component';
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import * as moment from 'moment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   @ViewChild(CalendarComponent)
   public curDay;
   public curDayFormat;
   public isSelected = true;
+  public showDate = true;
+  public weekdayFormat;
 
   title = 'calender';
+
+  ngOnInit(): void {
+  }
+
+  // changeFlagTrue() {
+  //   localStorage.setItem('isSelected', 'true');
+  //   localStorage.setItem('showDate', 'true');
+  // }
+  // changeFlagFalse() {
+  //   localStorage.setItem('isSelected', 'false');
+  //   localStorage.setItem('showDate', 'false');
+  // }
+  // getSelected(): string {
+  //   return localStorage.getItem('isSelected');
+  // }
+  // getShowDate(): string {
+  //   return localStorage.getItem('showDate');
+  // }
   onChangeDay(clickedDay) {
     this.curDay = clickedDay;
-    this.curDayFormat = this.curDay.format('MM/DD');
+    this.changeFormat();
   }
 
   previous() {
     this.curDay = this.curDay.subtract(1, 'd');
-    this.curDayFormat = this.curDay.format('MM/DD');
+    this.changeFormat();
   }
   next() {
     this.curDay = this.curDay.add(1, 'd');
-    this.curDayFormat = this.curDay.format('MM/DD');
+    this.changeFormat();
+
   }
   currentDay() {
     this.curDay = moment();
+    this.changeFormat();
+  }
+
+  changeFormat() {
     this.curDayFormat = this.curDay.format('MM/DD');
-    console.log(this.curDay);
+    this.weekdayFormat = this.curDay.format('ddd');
   }
 }
--- calender/src/app/weekly-schedule/weekly-schedule.component.ts
+++ calender/src/app/weekly-schedule/weekly-schedule.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as moment from 'moment';
 @Component({
   selector: 'app-weekly-schedule',
   templateUrl: './weekly-schedule.component.html',
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeeklyScheduleComponent implements OnInit {
 
+  public date = moment();
+  public curDay: string;
+  public timing;
+  public timingArr = new Array(24);
+  public weekday;
+  public weekdayArr = new Array(7);
+
   constructor() { }
 
   ngOnInit() {
+    this.curDay = this.date.format('L');
+    this.createTiming();
+    this.createWeekday();
   }
 
+  createTiming() {
+    for ( let i = 0; i < 10; i++) {
+      this.timingArr[i] = '0' + i + ':00';
+    }
+    for ( let i = 10; i < 24; i++) {
+      this.timingArr[i] = i + ':00';
+    }
+  }
+
+  createWeekday() {
+      this.weekdayArr[0] = 'SUN  ' + (moment().date() - moment().day());
+      this.weekdayArr[1] = 'MON  ' + (moment().date() - moment().day() + 1);
+      this.weekdayArr[2] = 'TUE  ' + (moment().date() - moment().day() + 2);
+      this.weekdayArr[3] = 'WED  ' + (moment().date() - moment().day() + 3);
+      this.weekdayArr[4] = 'THU  ' + (moment().date() - moment().day() + 4);
+      this.weekdayArr[5] = 'FRI  ' + (moment().date() - moment().day() + 5);
+      this.weekdayArr[6] = 'SAT  ' + (moment().date() - moment().day() + 6);
+  }
 }
