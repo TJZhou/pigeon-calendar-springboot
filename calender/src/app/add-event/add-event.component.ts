65d44b2325f62023fc6ac6fc4fcff5a00a767b81
elaineneu <yang.rui1@husky.neu.edu>
2019-04-13 01:22:08 -0400

    Create event model and update functions of two button.

--- calender/src/app/add-event/add-event.component.ts
+++ calender/src/app/add-event/add-event.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Event } from '../models/event.model';
 import * as moment from 'moment';
 
 @Component({
@@ -9,15 +11,54 @@ import * as moment from 'moment';
 export class AddEventComponent implements OnInit {
 
   currentDate = moment();
-  startDate = this.currentDate.format('MM/DD/YY');
-  startTime = this.currentDate.format('HH:MM');
-  endTime = this.currentDate.add(1, 'h').format('HH:MM');
-  endDate = this.startDate;
+  startDateTemp = this.currentDate.format('MM/DD/YY');
+  startTimeTemp = this.currentDate.format('HH:MM');
+  endTimeTemp = this.currentDate.add(1, 'h').format('HH:MM');
+  endDateTemp = this.startDateTemp;
 
-  
-  constructor() { }
+  event = Event;
+
+  id: number;
+  username: string;
+  title: string;
+  location: string;
+  startTime: Date;
+  endTime: Date;
+
+  constructor(private routeLocation: Location) { }
 
   ngOnInit() {
   }
 
+  createNewContact(): Event{
+    let event = {
+      "id": this.id,
+      "username": this.username,
+      "title": this.title,
+      "location": this.location,
+      "startTime": this.startTime,
+      "endTime": this.endTime
+    }
+    return event;
+  }
+
+  goBack(): void {
+    this.routeLocation.back();
+  }
+
+  onSubmit(){
+
+    if (this.title == "" || this.title == undefined || 
+        this.location == "" || this.location == undefined) {
+
+      alert("Please fill out all the blanks.");
+
+    } else {
+
+      // this.eventService.addEvent(this.createNewEvent())
+      //   .subscribe(data => console.log(" This event has been created: " + data));
+      // alert('Add successfully.');
+
+    }
+  }
 }
---
+++ calender/src/app/models/event.model.ts
@@ -0,0 +1,8 @@
+export class Event{
+    id: number;
+    username: string;
+    title: string;
+    // content: string;
+    startTime: Date;
+    endTime: Date
+}
