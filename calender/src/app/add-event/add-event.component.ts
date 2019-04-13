c8f9bed555bc307c1945916ba73c2e210097d615
elaineneu <yang.rui1@husky.neu.edu>
2019-04-13 16:45:22 -0400

    Create User and Event Services.

--- calender/src/app/add-event/add-event.component.ts
+++ calender/src/app/add-event/add-event.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { Event } from '../models/event.model';
+import { EventService } from '../services/event.service';
 import * as moment from 'moment';
+import { EventPost } from '../models/eventPost.model';
 
 @Component({
   selector: 'app-add-event',
@@ -33,7 +35,10 @@ export class AddEventComponent implements OnInit {
   selectedEndDate = null;
   selectedEndTime = null;
 
-  constructor(private routeLocation: Location) { }
+  constructor(
+    private routeLocation: Location,
+    private eventService: EventService
+    ) { }
 
   ngOnInit() {
   }
@@ -74,35 +79,46 @@ export class AddEventComponent implements OnInit {
     }
   }
 
-  createNewContact(): Event{
-    let event = {
-      "id": this.id,
-      "username": this.username,
-      "title": this.title,
-      "location": this.location,
-      "startTime": this.startTime,
-      "endTime": this.endTime
-    }
-    return event;
-  }
+  
 
   goBack(): void {
     this.routeLocation.back();
   }
 
+
+  createNewEvent(): EventPost{
+    let event = {
+    "username": "yujxie",
+    "title": "test",
+    "location": "testLocation",
+    "startTime": new Date(),
+    "endTime": new Date()
+    }
+    return event;
+  }
+
   onSubmit(){
 
-    if (this.title == "" || this.title == undefined || 
-        this.location == "" || this.location == undefined) {
+    // if (this.title == "" || this.title == undefined || 
+    //     this.location == "" || this.location == undefined) {
 
-      alert("Please fill out all the blanks.");
+    //   alert("Please fill out all the blanks.");
 
-    } else {
+    // } else {
+      
 
-      // this.eventService.addEvent(this.createNewEvent())
-      //   .subscribe(data => console.log(" This event has been created: " + data));
-      // alert('Add successfully.');
+      // let event = {
+      //   "username": "yujxie",
+      //   "title": "test",
+      //   "location": "testLocation",
+      //   "startTime": "2019-04-11T18:00:00.000Z",
+      //   "endTime": "2019-04-11T18:00:00.000Z"
+      // }
 
-    }
+      this.eventService.addEvent(this.createNewEvent())
+        .subscribe(data => console.log(" This event has been created: " + data));
+      alert('Add successfully.');
+
+    // }
   }
 }
---
+++ calender/src/app/models/eventPost.model.ts
@@ -0,0 +1,7 @@
+export class EventPost{
+    username: string;
+    title: string;
+    // content: string;
+    startTime: Date;
+    endTime: Date
+}
