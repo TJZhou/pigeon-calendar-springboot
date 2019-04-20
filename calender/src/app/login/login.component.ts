e4817edf30fe31ace9880aeee7ceaab195a83009
xieyuju <46663898+xieyuju@users.noreply.github.com>
2019-04-20 15:25:47 -0400

    add login event

--- calender/src/app/login/login.component.ts
+++ calender/src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { UserModel } from '../models/user.model';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import { UserService } from '../services/user.service'
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -14,14 +16,11 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private service: UserService) { }
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
-      email: [this.user.email, [
-        Validators.required,
-        Validators.email
-      ]],
+      username: [this.user.username],
       password: [this.user.password, [
         Validators.required,
         Validators.minLength(6),
@@ -31,7 +30,23 @@ export class LoginComponent implements OnInit {
   }
 
   onLoginSubmit() {
-    alert(this.user.email + ' ' + this.user.password);
+    
+    let currentUser = new UserModel();
+    this.service.getUser(this.user.username)
+      .subscribe(user => {
+        let currentUser = new UserModel();
+        currentUser = user;
+        localStorage.setItem("username", user.username);
+        localStorage.setItem("password", user.password);
+        localStorage.setItem("email", user.email);
+        console.log(currentUser)
+      });
+
+    console.log(this.user.password.valueOf());
+    console.log(localStorage.getItem("password"));
+
+    if(this.user.password.valueOf() == localStorage.getItem("password").valueOf())
+      alert(this.user.password + ' ' + this.user.password);
   }
 
 }
