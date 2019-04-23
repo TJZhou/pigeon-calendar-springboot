b743cb82d64bbfe19c81a7ec052fb8504ae8bb9f
Yuanxin Zhang <zhang.yuanx@husky.neu.edu>
2019-04-22 23:45:30 -0400

    Update register component.

--- calender/src/app/check-profile-details/check-profile-details.component.ts
+++ calender/src/app/check-profile-details/check-profile-details.component.ts
@@ -1,6 +1,6 @@
 import { UserService } from './../services/user.service';
 import { Component, Input, OnInit, Inject } from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 @Component({
   selector: 'app-check-profile-details',
@@ -12,7 +12,9 @@ export class CheckProfileDetailsComponent implements OnInit {
   @Input() id: string ;
   constructor(
     public dialogRef: MatDialogRef<CheckProfileDetailsComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any, private http: HttpClient) {}
+    @Inject(MAT_DIALOG_DATA) public data: any, private http: HttpClient) {
+
+    }
 
   onNoClick(): void {
     this.dialogRef.close();
