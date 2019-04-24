ef6be4c9a9dd84c11142d56f357027f1fd72f0f4
Tianju Zhou <zhou.tianj@husky.neu.edu>
2019-04-24 15:20:56 -0400

    add invalid information

--- calender/src/app/login/login.component.ts
+++ calender/src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router} from '@angular/router';
 
 import { UserService } from '../services/user.service'
+import { validateConfig } from '@angular/router/src/config';
 
 @Component({
   selector: 'app-login',
@@ -15,36 +16,54 @@ export class LoginComponent implements OnInit {
   user: UserModel = new UserModel();
   isShow: boolean;
   loginForm: FormGroup;
-
+  public valid1 = true;
+  public valid2 = true;
 
   constructor(private formBuilder: FormBuilder, private service: UserService, private router: Router) { }
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
       username: [this.user.username],
-      password: [this.user.password, [
-        Validators.required,
-        Validators.minLength(6),
-        Validators.maxLength(30)
-      ]]
+      password: [this.user.password,
+      //    [
+      //   // Validators.required,
+      //   // Validators.minLength(6),
+      //   // Validators.maxLength(30)
+      // ]
+    ]
     });
   }
 
   onLoginSubmit() {
 
-    let currentUser = new UserModel();
-    this.service.getUser(this.user.username)
-      .subscribe(user => {
-        console.log(user)
-        console.log(user[0])
-        localStorage.setItem("username", user[0].username);
-        localStorage.setItem("password", user[0].password);
-        localStorage.setItem("email", user[0].email);
-        if(this.user.password === user[0].password){
-          this.router.navigateByUrl("day");
-        }
-      });
+    if (this.user.username === undefined || this.user.password === undefined || this.user.password.length < 6 ) {
+      this.valid2 = false;
+      this.valid1 = true;
+    } else {
+      let currentUser = new UserModel();
+      this.service.getUser(this.user.username)
+        .subscribe(user => {
+          // console.log(user)
+          // console.log(user[0])
+          localStorage.setItem("username", user[0].username);
+          localStorage.setItem("password", user[0].password);
+          localStorage.setItem("email", user[0].email);
+          if(this.user.password === user[0].password){
+            this.router.navigateByUrl("day");
+            this.valid1 = true;
+            this.valid2 = true;
+          } else {
+            this.valid1 = false;
+            this.valid2 = true;
+          }
+        });
+    }
+  }
 
-    
+  isValid1() {
+    return this.valid1;
+  }
+  isValid2() {
+    return this.valid2;
   }
 }
--- calender/src/app/register/register.component.ts
+++ calender/src/app/register/register.component.ts
@@ -19,36 +19,43 @@ export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   passwordComfirm: string;
   isExist = false;
-
+  public valid = true;
 
   constructor(private formBuilder: FormBuilder, private service: UserService, private router: Router) { }
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
       username: [this.user.username],
-      email: [this.user.email, [
-          Validators.required,
-          Validators.email
-        ]
+      email: [this.user.email,
+        // [
+        //   Validators.required,
+        //   Validators.email
+        // ]
       ],
-      password: [this.user.password, [
-          Validators.required,
-          Validators.minLength(6),
-          Validators.maxLength(30)
-        ]
+      password: [this.user.password,
+        // [
+        //   Validators.required,
+        //   Validators.minLength(6),
+        //   Validators.maxLength(30)
+        // ]
       ],
-      passwordComfirm: [this.passwordComfirm, [
-          Validators.required,
-          Validators.minLength(6),
-          Validators.maxLength(30)
-        ]
+      passwordComfirm: [this.passwordComfirm,
+        // [
+        //   Validators.required,
+        //   Validators.minLength(6),
+        //   Validators.maxLength(30)
+        // ]
       ]
 
     });
   }
 
   onRegisterSubmit() {
-    this.service.getUser(this.user.username)
+    if ( this.user.email === undefined || this.user.password === undefined || this.passwordComfirm === undefined
+      || this.user.password.length < 6) {
+      this.valid = false;
+    } else {
+      this.service.getUser(this.user.username)
       .subscribe( user => {
         if (user[0] != null && user[0].username === this.user.username) {
           alert ('This username is registered!');
@@ -58,10 +65,13 @@ export class RegisterComponent implements OnInit {
             this.service.addUser(this.user)
               .subscribe();
             this.router.navigateByUrl('login');
-          } else { alert('Passwords must match!'); }
+          } else { this.valid = false; }
         }
       });
-
+    }
   }
 
+  public isValid() {
+    return this.valid;
+  }
 }
