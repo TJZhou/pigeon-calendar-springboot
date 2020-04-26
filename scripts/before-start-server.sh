9bf904970078936f72558ab8c0de2e8af4e9bc16
Tianju Zhou <zhou.tianj@husky.neu.edu>
2020-04-26 17:54:29 -0400

    update EC2 server deployment scripts

--- scripts/before-start-server.sh
+++ scripts/before-start-server.sh
@@ -5,4 +5,5 @@ docker rmi tjzhou/pigeon:2.0
 docker pull tjzhou/pigeon:2.0
 
 # remove old assets file
+tar -czvf /var/www/pigeon-frontend-backup.tar.gz /var/www/calendar
 rm -rf /var/www/calendar/*
